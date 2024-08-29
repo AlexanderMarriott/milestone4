e862a0e0a16ebf276f4e44e0a6f4cdd6259748f1
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-08-29 17:32:21 +0100

    imprted includes. created path for shop.urls

--- milestone4/milestone4/urls.py
+++ milestone4/milestone4/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("shop.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
