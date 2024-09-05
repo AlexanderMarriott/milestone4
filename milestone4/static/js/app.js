b5d420df32dcfab7f595e536121c50f3ed4a42a4
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-05 16:18:35 +0100

    chore: Refactor app.js to use variables for URL and CSRF token

--- milestone4/static/js/app.js
+++ milestone4/static/js/app.js
@@ -1,21 +1,20 @@
-    $(document).on('click', '#add-button', function (e) {
+// app.js
+$(document).on('click', '#add-button', function (e) {
     e.preventDefault();
     $.ajax({
         type: 'POST',
-        url: "{% url 'basket_add' %}",
+        url: basketAddUrl,  // Use a variable for the URL
         data: {
             product_id: $('#add-button').val(),
             quantity: $('#select option:selected').text(),
-            csrfmiddlewaretoken: '{{ csrf_token }}',
+            csrfmiddlewaretoken: csrfToken,  // Use a variable for the CSRF token
             action: 'post'
         },
-
         success: function (json) {
-
+            console.log(json);
         },
-
         error: function (xhr, errmsg, err) {
-
+            // Handle error
         }
     });
 });
