4e29c338e85a3edb68c34129cf5043c81495676a
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-05 10:27:25 +0100

    Refactor URL patterns in shop/urls.py for main page, product detail page, and category page

--- milestone4/shop/urls.py
+++ milestone4/shop/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # main page
     path("", views.store, name="shop"),
+    # product detail page
     path("product/<slug:slug>/", views.product_detail, name="product_detail"),
+    # category page
+    path("search/<slug:category_slug>/", views.list_category, name="list_category"),
 ]
