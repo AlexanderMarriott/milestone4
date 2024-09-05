5b6ca6e83f4d5f16cf28f5c1ea7a01850cba51f9
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-05 10:14:17 +0100

    added list category view to views.py

--- milestone4/shop/views.py
+++ milestone4/shop/views.py
@@ -20,6 +20,13 @@ def categories(requests):
     return {"all_categories": all_categories}
 
 
+def list_category(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    context = {"category": category, "products": products}
+    return render(request, "shop/list_category.html", context)
+
+
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     context = {"product": product}
