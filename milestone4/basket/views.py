aa1a8d620fbf0281c9073db47e6d407e6729919a
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-05 12:16:09 +0100

    chore: Refactor basket.py, add AJAX functionality, and update product_detail.html

--- milestone4/basket/views.py
+++ milestone4/basket/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 
+from .basket import basket
+
+from shop.models import Product
+
+from django.shortcuts import get_object_or_404
+
 
 def basket_summary(request):
     return render(request, "basket/basket_summary.html")
 
 
 def basket_add(request):
-    pass
+    basket = basket(request)
+
+    if request.post.get("action") == "post":
+        product_id = int(request.post.get("product_id"))
+        quantity = int(request.post.get("quantity"))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        basket.add(product=product, quantity=quantity)
 
 
 def basket_delete(request):
