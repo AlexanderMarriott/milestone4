da676fdd11cffe81bfb6099937e694bea272540e
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-20 14:02:07 +0100

    chore: Add delete functionality for basket items in app.js

--- milestone4/static/js/app.js
+++ milestone4/static/js/app.js
@@ -1,6 +1,7 @@
 // app.js
 
 
+// add button
 $(document).on('click', '#add-button', function (e) {
     e.preventDefault();
     $.ajax({
@@ -15,6 +16,31 @@ $(document).on('click', '#add-button', function (e) {
         success: function (json) {
             //console.log(json);
             document.getElementById('basket-qty').textContent = json.qty;
+        },
+        error: function (xhr, errmsg, err) {
+            // Handle error
+        }
+    });
+});
+
+// delete button
+$(document).on('click', '.delete-button', function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: basketDeleteUrl,  // Use a variable for the URL
+        data: {
+            product_id: $(this).data('index'),
+            csrfmiddlewaretoken: csrfToken,  // Use a variable for the CSRF token
+            action: 'post'
+        },
+        success: function (json) {
+            //console.log(json);
+
+           location.reload()
+           
+           document.getElementById('basket-qty').textContent = json.qty
+           document.getElementById('total').textContent = json.total
         },
         error: function (xhr, errmsg, err) {
             // Handle error
