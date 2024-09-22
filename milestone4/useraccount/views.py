f35b5d2d42cf02166d3c8b5f033afa2bc6ac04f8
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-22 13:08:39 +0100

    fix: Update register view to render registration template and handle exceptions

--- milestone4/useraccount/views.py
+++ milestone4/useraccount/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
-
 def register(request):
-    return HttpResponse('Account url setup')
+    try:
+        return render(request, 'useraccount/registration/register.html')
+    except Exception as e:
+        return HttpResponse(f"Error: {e}")
