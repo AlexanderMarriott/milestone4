4d64ae0bb212eb5261923fb52c87ce3df0492b4d
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-24 17:55:10 +0100

    feat: Update email settings configuration and improve environment variable usage

--- milestone4/milestone4/settings.py
+++ milestone4/milestone4/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,8 +146,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER') # email address used to send emails
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD') # email password
