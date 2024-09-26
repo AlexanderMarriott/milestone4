ec876729c2d6459e35e0388873f1b530065f69ca
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-26 18:19:07 +0100

    feat: Add profile management and delete account routes to user account URLs

--- milestone4/useraccount/urls.py
+++ milestone4/useraccount/urls.py
@@ -21,7 +21,20 @@ urlpatterns = [
 
     path('user-logout/', views.user_logout, name='user-logout'),
 
+   
+
+    #dashboard/profile
+
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('profile-management/', views.profile_management, name='profile-management'),
+
+    path('delete-account/', views.delete_account, name='delete-account'),
+
+
+        
+
+
 
     
 ]
