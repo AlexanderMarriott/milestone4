a7df5a81a62bbb209c5cdd747ad0d00fc350a1cc
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-28 16:16:54 +0100

    feat: Implement prefilled checkout form with user's shipping information

--- milestone4/payments/views.py
+++ milestone4/payments/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
+from .models import ShippingAddress
+
+def checkout(request):
+
+    #prefill the form with the user's saved info
+
+    if request.user.is_authenticated:
+
+        try:
+            #authenticated user WITH shipping info
+
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+
+            context = {
+                "shipping": shipping_address,
+            }
+
+            return render(request, "payments/checkout.html", context=context)
+
+        except:
+
+            #authenticated user WITHOUT shipping info
+            return render(request, "payments/checkout.html")
+
+            
+    else:
+
+        return render(request, "payments/checkout.html")
 
 
 def payment_success(request):
@@ -6,3 +34,4 @@ def payment_success(request):
 
 def payment_failed(request):
     return render(request, "payments/payment-failed.html")
+
