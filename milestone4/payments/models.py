4c8113805bc7efaa22b70bd1e1a03d0366175c71
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-28 20:39:36 +0100

    feat: Add Order and OrderItem models with relationships to User and Product

--- milestone4/payments/models.py
+++ milestone4/payments/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-from django.db import models
-
-from django.utils import timezone
+from shop.models import Product
 
 class ShippingAddress(models.Model):
     first_name = models.CharField(max_length=30)
@@ -26,6 +24,37 @@ class ShippingAddress(models.Model):
     def __str__(self):
         if self.user:
             return 'Shipping Address for {}'.format(self.user.username)
-        return 'Shipping Address for an anonymous user'
+        else:
+
+            return 'Shipping Address for an anonymous user'
 
 # Create your models here.
+
+class Order(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=1000)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    #foreign key to user
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        
+        return 'order - #' + str(self.id)
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+        #foreign key to user
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order Item for order #' + str(self.order.id) + ' - ' + self.product.title
+
