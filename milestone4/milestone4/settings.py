12ba3bc561d1a9456c8fa6566ec01fd01e1c3b83
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-29 14:13:43 +0100

    fix: Add STATICFILES_DIRS to settings and remove requirements.txt

--- milestone4/milestone4/settings.py
+++ milestone4/milestone4/settings.py
@@ -146,6 +146,10 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "static/media"
