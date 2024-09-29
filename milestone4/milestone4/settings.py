e5aad1c6c94248d30408f021dd41731d2d428801
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-29 17:48:49 +0100

    refactor: Update STATIC_ROOT and MEDIA_ROOT paths for consistency

--- milestone4/milestone4/settings.py
+++ milestone4/milestone4/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Example app static directory
@@ -154,7 +154,9 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = BASE_DIR / "static/media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
