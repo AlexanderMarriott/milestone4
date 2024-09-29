b3e17296989d09bfbdb73ac0a669b6846f237130
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-09-29 17:57:19 +0100

    feat: Add static URL configuration for serving media files

--- milestone4/shop/urls.py
+++ milestone4/shop/urls.py
@@ -1,5 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,4 @@ urlpatterns = [
     path("product/<slug:slug>/", views.product_detail, name="product_detail"),
     # category page
     path("search/<slug:category_slug>/", views.list_category, name="list_category"),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
